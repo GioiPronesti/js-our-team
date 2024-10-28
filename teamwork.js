ba31994530b16dbb07c9f8e5af6fba99a7aa241d
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-28 17:45:06 +0100

    add new card dom

--- teamwork.js
+++ teamwork.js
@@ -24,7 +24,74 @@ const teamMembers = [
 
   aggiungere con js le card in pagina e inserire i dati nell array per ogni card soltanto alla fine.  
 
-  
+  2. Copiate il codice di una card (ovviamente commentate le card lato HTML) e con JS stampate per 6 volte (tanti quanti sono gli elementi dell'array) la stessa card
 
+    3. Modificate il codice della singola card in modo che abbia al suo interno ogni volta i dati del singolo componente del team
+
+  card result example 
+  
+  img 
+    name: "Marco Bianchi"
+    role: "Designer"
+    email: "marcobianchi@team.com"
 
 */
+
+//const newSectionElement = document.getElementById("section-cards");
+
+const newCardElement = document.getElementById("team-card");
+
+const newElementItem = document.getElementById("team-property");
+
+/*
+1) pre prendere gli elementi dell' array delle card dobbiamo usare il 
+   ciclo for
+*/
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
+
+const newDomMerbersTeam = document.getElementById("new-members-team");
+
+for (i = 0; i < teamMembers.length; i++) {
+  const currentMember = teamMembers[i];
+  console.log(i, currentMember);
+
+  newCardElement.innerHTML += `<div class="team-card"> card </div>`;
+}
