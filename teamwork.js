f0f35b0bc0b6d0279fb89295ec637947deee7b9d
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-10-28 16:32:17 +0100

    first commit inizial step

---
+++ teamwork.js
@@ -0,0 +1,30 @@
+/*
+Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+(trovate l’array del team all’interno della cartella in allegato)
+Bonus
+Rendere l’esercizio responsive, mandando a capo le card
+Aggiungere, sotto alla sezione dei membri del team, un form di aggiunta membri che permetta di aggiungere un nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! 
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+ 
+  ....
+  ]
+
+  aggiungere con js le card in pagina e inserire i dati nell array per ogni card soltanto alla fine.  
+
+  
+
+
+*/
